51104d5606b1afe996d0c8fc2d04f4f2a12e84f2
fesmjke <fatalerror.sm@gmail.com>
2020-06-06 03:53:13 +0300

    refactored to class structure

--- bio_characteristic.py
+++ bio_characteristic.py
@@ -1,34 +1,34 @@
 from random import randint
 
+class BIO:
+    __ages = []
+    __merge_bio = []
+    def __random_genders(self,player_number):
+        return [randint(0, 1) for i in range(0, player_number)]
 
-def random_genders():
-    return [randint(0, 1) for i in range(0, 10)]
+    def __random_ages(self,player_number):
+        for i in range(0,player_number//2):
+            current_age = randint(18,80)
+            self.__ages.append(current_age)
+            if current_age >= 50:
+                next_age = randint(18,50)
+                self.__ages.append(next_age)
+                continue
+            if current_age <= 50:
+                next_age = randint(50,80)
+                self.__ages.append(next_age)
+                continue
+        return self.__ages
 
+    def __merge(self,player_number):
+        genders = self.__random_genders(player_number)
+        ages = self.__random_ages(player_number)
+        for i in range(0,10):
+            if genders[i] == 0:
+                self.__merge_bio.append({"Мужчина": f"{ages[i]}"})
+            else:
+                self.__merge_bio.append({"Женщина": f"{ages[i]}"})
 
-def random_ages():
-    ages = []
-    for i in range(0,5):
-        current_age = randint(18,80)
-        ages.append(current_age)
-        if current_age >= 50:
-            next_age = randint(18,50)
-            ages.append(next_age)
-            continue
-        if current_age <= 50:
-            next_age = randint(50,80)
-            ages.append(next_age)
-            continue
-    return ages
-
-
-def flow():
-    genders = random_genders()
-    ages = random_ages()
-    merge = {}
-    for i in range(0,10):
-        if genders[i] == 0:
-            print("Male -", ages[i])
-        else:
-            print("Female -", ages[i])
-
-flow()
+    def get_bio_info(self,player_number):
+        self.__merge(player_number)
+        return self.__merge_bio
